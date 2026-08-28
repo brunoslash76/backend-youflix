eab14fb00537f97ba0e29ca2092daf1cfca1d40a
Bruno T M Russo <bruno@irango.com.br>
2026-08-28 17:34:01 -0300

    feat: add jwt security

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,32 @@
+import { UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import type { FastifyRequest } from 'fastify';
+import { type JwtPayload } from 'jsonwebtoken';
+import { ExtractJwt, Strategy } from "passport-jwt";
+
+const JWT_SECRET = process.env.JWT_SECRET!;
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    if (!JWT_SECRET) throw new Error('JWT_SECRET is not set');
+
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: FastifyRequest) => {
+          return request?.cookies?.access_token || null
+        }
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: JWT_SECRET
+    })
+  }
+
+  async validate(payload: JwtPayload) {
+    if (!payload) throw new UnauthorizedException()
+
+    return {
+      userId: payload.sub,
+      email: payload.email,
+    }
+  }
+}
