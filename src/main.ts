63c1d54d56b2345c525d1952c6832888631bb7f5
Bruno T M Russo <bruno@irango.com.br>
2026-08-28 17:30:04 -0300

    feat: add fastify, cookies and validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
+import fastifyCookie from '@fastify/cookie';
 import helmet from '@fastify/helmet';
-import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { config } from '../src/config/index.js';
 import { AppModule, ObserveInstrument } from './app.module.js';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard.js';
+
+const COOKIE_SECRET = config.cookie.secret;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
     instrument: ObserveInstrument,
   });
+
+  await app.register(fastifyCookie, {
+    secret: COOKIE_SECRET,
+  })
 
   app.register(helmet, {
     contentSecurityPolicy: {
@@ -20,15 +30,22 @@ async function bootstrap() {
     }
   })
 
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
+
+  const reflector = app.get(Reflector)
+  app.useGlobalGuards(new JwtAuthGuard(reflector))
+
   const config = new DocumentBuilder()
     .setTitle('YouFlix API')
     .setDescription('API for YouFlix')
     .setVersion('1.0')
     .build();
-  
+
   const documentFactory = () => SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, documentFactory())
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
+  });
 }
 await bootstrap();
